9343307700408403c94e91dab5fc6300b38c443a
mgapsa <gapsamikolaj@gmail.com>
2017-10-23 19:48:49 +0200

    Testing

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,8 +1,16 @@
+#include <bitset>
 #include "Logger.hpp"
 
 void printVector(std::string name, const std::vector<char> vect) {
     std::cout << name << " = ";
     for (const auto i : vect)
         std::cout << i << ", ";
+    std::cout << "\n";
+}
+
+void printVectorAsBits(std::string name, const std::vector<char> vect){
+    std::cout << name << " = ";
+    for (const auto i : vect)
+        std::cout<<std::bitset<8>(i)<<std::endl;
     std::cout << "\n";
 }
--- src/codec.cpp
+++ src/codec.cpp
@@ -11,6 +11,19 @@ int main() {
                                 'a', 'b', 'd'};
     printVector("Source", source);
     auto comp = codec.encode(source);
+    printVectorAsBits("Encoded", comp);
+    //jezeli odnalazl to od zera zaczyna
+    /*
+    01100001 -> a
+    00010    -> 0 0 2
+    00110    -> 0 1 2
+    00100    -> 0 1 0
+    110100110 -> 1 ¦ ??? 
+     001100100000100001100100
+     */
+    //a
+    //0 0 2
+    //
     auto dec = codec.decode(comp);
     printVector("Decoded", dec);
 
